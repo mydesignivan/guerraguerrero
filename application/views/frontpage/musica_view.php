b4690137bc6f8c6a99a8b030adaf25d8828be617
root <root@juan-laptop.(none)>
2010-09-27 01:41:10 -0300

    ultimos cambios antes que me explote el cerebro

--- application/views/frontpage/musica_view.php
+++ application/views/frontpage/musica_view.php
@@ -1,3 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
 
 <?=@$content?>
+<br />
+<br />
+<div class="music_menu"><a href="/musicaeventilum">Eventos</a><div class="arrow"><img src="../../img/arrow-menu.png" alt="" /></div></div>
+<div class="music_menu"><a href="/musicadiscoteque">Discoteque</a><div class="arrow"><img src="../../img/arrow-menu.png" alt="" /></div></div>
+<div class="music_menu"><a href="/musicaradio">Radios</a><div class="arrow"><img src="../../img/arrow-menu.png" alt="" /></div></div>
--- application/views/frontpage/musicaeventilum_view.php
+++ application/views/frontpage/musicaeventilum_view.php
@@ -1,3 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
 
 <?=@$content?>
+<div class="music_menu"><a href="/musicadiscoteque">Discoteque</a><div class="arrow"><img src="../../img/arrow-menu.png" alt="" /></div></div>
+<div class="radios_menu"><a href="/musicaradio">Radios</a></div>
