09fe1f839648c121ec626e2d4855381566320587
root <root@juan-laptop.(none)>
2010-09-30 08:03:50 -0300

    cambios

--- application/views/frontpage/descargatumusica_view.php
+++ application/views/frontpage/descargatumusica_view.php
@@ -5,7 +5,7 @@
 $src = UPLOAD_PATH_MP3 . $row['filename'];
 ?>
     <li>
-        <div class="cell1"><a href="<?=$src?>"><img src="img/download.png" alt="" width="22" height="21" /></a></div>
+        <div class="cell1"><a href="<?=$src?>"><img src="img/download.png" alt="" width="15" height="15" /></a></div>
         <div class="cell2"><a href="<?=$src?>"><?=$row['name']?></a></div>
     </li>
 <?php }?>
--- application/views/includes/footer_inc.php
+++ application/views/includes/footer_inc.php
@@ -2,10 +2,10 @@
 
 <div class="clear span-100 footer last">
     <?php if( $this->uri->segment(1)=="" || $this->uri->segment(1)=="index" ){?>
-    <img src="img/footer-slogan-800.jpg" alt="" width="100%" />
+    <img src="img/footer-slogan.jpg" alt="" width="100%" />
     <?php }else{?>
-    <img src="img/footer-800.jpg" alt="" width="100%" />
+    <img src="img/foot.jpg" alt="" width="100%" />
     <?php }?>
     <p>info@guerraguerrero.com<br />Tel: 154542850 / 155048282 / 145*4588</p>
-    <p class="copyright"><br />2010 (&copy;) Copyright<br />Dise&ntilde;o y Desarrollo <img src="../../img/logo-neuronas-pie.jpg" alt="Neuronas SA" /></p>
+    <p class="copyright"><br />2010 (&copy;) Copyright<br />Dise&ntilde;o y Desarrollo <a href="http://m.neuronasa.com"><img src="../../img/logo-neuronas-pie.jpg" alt="Neuronas SA" /></a></p>
 </div>
